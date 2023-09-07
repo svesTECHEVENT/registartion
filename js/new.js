26dcede4939f00d4dda29a0a590db4cec40f5295
svesTECHEVENT <144025248+svesTECHEVENT@users.noreply.github.com>
2023-09-07 14:31:19 +0530

    Add files via upload

---
+++ js/new.js
@@ -0,0 +1,23 @@
+function fun(){
+    var a=document.getElementById("con");
+    a.style.color="var(--secondary-color)";
+      var b=document.getElementById("con4");
+      b.style.color="white";
+}
+function fun1(){
+    var a=document.getElementById("con");
+    a.style.color="white";
+}
+function fun2(){
+    var a=document.getElementById("con");
+    a.style.color="white";
+}
+function fun3(){
+    var a=document.getElementById("con");
+    a.style.color="white";
+}
+function fun4(){
+    var a=document.getElementById("con");
+    var b=document.getElementById("con4");
+    a.style.color="white";
+}
